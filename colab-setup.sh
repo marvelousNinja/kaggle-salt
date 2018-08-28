eba91b8f27960864ffd063e0d266af59ff136e52
Alexander Sologub <alexsologoub@gmail.com>
2018-08-29 00:19:40 +0300

    Add missing deps

--- colab-setup.sh
+++ colab-setup.sh
@@ -6,6 +6,5 @@ apt-get install -y python3.6-tk nano htop screen nvidia-cuda-toolkit --no-instal
 pip3 install http://download.pytorch.org/whl/cu80/torch-0.4.1-cp36-cp36m-linux_x86_64.whl
 pip3 install -r requirements.txt
 
-./make.sh
 cp kaggle.json.sample kaggle.json
 cp telegram.conf.sample telegram.conf
