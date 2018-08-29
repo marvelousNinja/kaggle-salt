d04567c3791db0ab8dca4615ad125e7c2eeb9bbe
Alexander Sologub <alexsologoub@gmail.com>
2018-08-29 22:04:43 +0300

    Remove image label processing from predict task

--- salt/predict.py
+++ salt/predict.py
@@ -4,7 +4,7 @@ import torch
 from fire import Fire
 from tqdm import tqdm
 
-from salt.model_checkpoint import load_checkpoint
+from salt.callbacks.model_checkpoint import load_checkpoint
 from salt.generators import get_test_generator
 from salt.utils import as_cuda
 from salt.utils import encode_rle
@@ -24,14 +24,11 @@ def predict(checkpoint_path, batch_size=1, limit=None):
     test_generator = get_test_generator(batch_size, limit)
     for inputs, _ in tqdm(test_generator, total=len(test_generator)):
         inputs = from_numpy(inputs)
-        mask_logits, image_logits = model(inputs)
-        masks = to_numpy(torch.argmax(mask_logits, dim=1))
-        image_logits = to_numpy(image_logits)
-        image_logits -= np.expand_dims(np.max(image_logits, axis=1), axis=1)
-        image_probs = (np.exp(image_logits) / np.expand_dims(np.sum(np.exp(image_logits), axis=1), axis=1))[:, 1]
-        for mask, prob in zip(masks, image_probs):
+        outputs = model(inputs)
+        masks = to_numpy(torch.argmax(outputs, dim=1))
+        for mask in masks:
             _id = ids.pop(0)
-            if prob > 0.8:
+            if mask.max() == 0:
                 records.append((_id, None))
             else:
                 records.append((_id, encode_rle(resize((101, 101), mask))))
