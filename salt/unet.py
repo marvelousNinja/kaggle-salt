13f5461dda6c31d0e03297bd1d65c839b9a5c152
Alexander Sologub <alexsologoub@gmail.com>
2018-09-02 17:40:59 +0300

    Properly register lists of layers

--- salt/unet.py
+++ salt/unet.py
@@ -35,19 +35,19 @@ class Unet(torch.nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.maxpool = torch.nn.MaxPool2d(2)
-        self.encoders = [
+        self.encoders = torch.nn.ModuleList([
             Encoder(1, 64, 3, 1),
             Encoder(64, 128, 3, 1),
             Encoder(128, 256, 3, 1),
             Encoder(256, 512, 3, 1)
-        ]
+        ])
 
-        self.decoders = [
+        self.decoders = torch.nn.ModuleList([
             Decoder(512, 1024, 512, 3, 1),
             Decoder(1024, 512, 256, 3, 1),
             Decoder(512, 256, 128, 3, 1),
             Decoder(256, 128, 64, 3, 1)
-        ]
+        ])
 
         self.final = torch.nn.Sequential(
             torch.nn.Conv2d(128, 64, 3, padding=1),
@@ -72,21 +72,21 @@ class UnetVgg16(torch.nn.Module):
         super().__init__()
         self.vgg = torchvision.models.vgg16(pretrained=False).features
         self.maxpool = torch.nn.MaxPool2d(2)
-        self.encoders = [
+        self.encoders = torch.nn.ModuleList([
             torch.nn.Sequential(self.vgg[0:4]),   # 64
             torch.nn.Sequential(self.vgg[5:9]),   # 128
             torch.nn.Sequential(self.vgg[10:16]), # 256
             torch.nn.Sequential(self.vgg[17:23]), # 512
             torch.nn.Sequential(self.vgg[24:30])  # 512
-        ]
+        ])
 
-        self.decoders = [
+        self.decoders = torch.nn.ModuleList([
             Decoder(512, 512, 256, 3, 1),         # x5 (512)
             Decoder(512 + 256, 512, 256, 3, 1),   # x5 (512) + d5 (256)
             Decoder(512 + 256, 512, 256, 3, 1),   # x4 (512) + d4 (256)
             Decoder(256 + 256, 256, 128, 3, 1),   # x3 (256) + d3 (256)
             Decoder(128 + 128, 128, 64, 3, 1)     # x2 (128) + d2 (128)
-        ]
+        ])
 
         self.final = torch.nn.Sequential(
             torch.nn.Conv2d(128, 64, 3, padding=1),
@@ -112,21 +112,21 @@ class UnetVgg11(torch.nn.Module):
         super().__init__()
         self.vgg = torchvision.models.vgg11(pretrained=False).features
         self.maxpool = torch.nn.MaxPool2d(2)
-        self.encoders = [
+        self.encoders = torch.nn.ModuleList([
             torch.nn.Sequential(self.vgg[0:2]),   # 64
             torch.nn.Sequential(self.vgg[3:5]),   # 128
             torch.nn.Sequential(self.vgg[6:10]),  # 256
             torch.nn.Sequential(self.vgg[11:15]), # 512
             torch.nn.Sequential(self.vgg[16:20])  # 512
-        ]
+        ])
 
-        self.decoders = [
+        self.decoders = torch.nn.ModuleList([
             Decoder(512, 512, 256, 3, 1),         # x5 (512)
             Decoder(512 + 256, 512, 256, 3, 1),   # x5 (512) + d5 (256)
             Decoder(512 + 256, 512, 256, 3, 1),   # x4 (512) + d4 (256)
             Decoder(256 + 256, 256, 128, 3, 1),   # x3 (256) + d3 (256)
             Decoder(128 + 128, 128, 64, 3, 1)     # x2 (128) + d2 (128)
-        ]
+        ])
 
         self.final = torch.nn.Sequential(
             torch.nn.Conv2d(128, 64, 3, padding=1),
@@ -135,7 +135,6 @@ class UnetVgg11(torch.nn.Module):
         )
 
     def forward(self, x):
-        import pdb; pdb.set_trace()
         x1 = self.encoders[0](x)
         x2 = self.encoders[1](self.maxpool(x1))
         x3 = self.encoders[2](self.maxpool(x2))
@@ -153,7 +152,7 @@ class UnetResnet34(torch.nn.Module):
         super().__init__()
         self.resnet = torchvision.models.resnet34(pretrained=False)
         self.maxpool = torch.nn.MaxPool2d(2)
-        self.encoders = [
+        self.encoders = torch.nn.ModuleList([
             torch.nn.Sequential(
                 self.resnet.conv1,
                 self.resnet.bn1,
@@ -163,16 +162,16 @@ class UnetResnet34(torch.nn.Module):
             self.resnet.layer2,  # 128
             self.resnet.layer3,  # 256
             self.resnet.layer4   # 512
-        ]
+        ])
 
-        self.decoders = [
+        self.decoders = torch.nn.ModuleList([
             Decoder(512, 512, 256, 3, 1),         # x5 (512)
             Decoder(512 + 256, 512, 256, 3, 1),   # x5 (512) + d5 (256)
             Decoder(256 + 256, 512, 256, 3, 1),   # x4 (256) + d4 (256)
             Decoder(128 + 256, 256, 128, 3, 1),   # x3 (128) + d3 (256)
             Decoder(64 + 128, 128, 64, 3, 1),     # x2 (64) + d2 (128)
             Decoder(64 + 64, 128, 64, 3, 1)       # x1 (64) + d1 (64)
-        ]
+        ])
 
         self.final = torch.nn.Sequential(
             torch.nn.Conv2d(64, num_classes, 1)
@@ -220,7 +219,7 @@ class ResidualUnet(torch.nn.Module):
         super().__init__()
         self.resnet = torchvision.models.resnet34(pretrained=False)
         self.maxpool = torch.nn.MaxPool2d(2)
-        self.encoders = [
+        self.encoders = torch.nn.ModuleList([
             torch.nn.Sequential(
                 self.resnet.conv1,
                 self.resnet.bn1,
@@ -230,16 +229,16 @@ class ResidualUnet(torch.nn.Module):
             self.resnet.layer2,  # 128
             self.resnet.layer3,  # 256
             self.resnet.layer4   # 512
-        ]
+        ])
 
-        self.decoders = [
+        self.decoders = torch.nn.ModuleList([
             ResidualDecoder(512, 256, 3, 1),         # x5 (512)
             ResidualDecoder(512 + 256, 256, 3, 1),   # x5 (512) + d5 (256)
             ResidualDecoder(256 + 256, 256, 3, 1),   # x4 (256) + d4 (256)
             ResidualDecoder(128 + 256, 128, 3, 1),   # x3 (128) + d3 (256)
             ResidualDecoder(64 + 128, 64, 3, 1),     # x2 (64) + d2 (128)
             ResidualDecoder(64 + 64, 64, 3, 1)       # x1 (64) + d1 (64)
-        ]
+        ])
 
         self.final = torch.nn.Sequential(
             torch.nn.Conv2d(64, num_classes, 1)
